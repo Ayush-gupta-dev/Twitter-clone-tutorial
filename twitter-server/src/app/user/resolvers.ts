cbf6569b01b32500c04fbca682a1a1bfbe0dc0dc
ayush-gupta-dev <ayushguptadev1@gmail.com>
2023-10-17 23:00:16 +0530

    backend/ interface of token result

--- twitter-server/src/app/user/resolvers.ts
+++ twitter-server/src/app/user/resolvers.ts
@@ -1,12 +1,32 @@
 import axios from "axios";
 
+interface GoogleTokenResult{
+    iss?: string; 
+    nbf?: string; 
+    aud?: string; 
+    sub?: string; 
+    email?: string; 
+    email_verified?: string; 
+    azp?: string; 
+    name?: string; 
+    picture?: string; 
+    given_name?: string; 
+    family_name?:  string;
+    iat?: string; 
+    exp?: string; 
+    jti?: string; 
+    alg?: string; 
+    kid?: string; 
+    typ?: string; 
+}
+
 const queries = {
     verifyGoogleToken: async(parent:any,{token}:{token: string})=>{
         const googleToken = token;
         const googleOauthUrl = new URL('https://oauth2.googleapis.com/tokeninfo')
         googleOauthUrl.searchParams.set('id_token',googleToken)
 
-        const {data} = await axios.get(googleOauthUrl.toString(),{
+        const {data} = await axios.get<GoogleTokenResult>(googleOauthUrl.toString(),{
             responseType : 'json'
         })
 
